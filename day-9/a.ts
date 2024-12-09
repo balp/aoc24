1c879678e272e8d5df3fe9a18a84fb2f34af83e3
Anders Arnholm <anders.arnholm@hiq.se>
2024-12-09 11:49:30 +0100

    day 9

--- day-9/a.ts
+++ day-9/a.ts
@@ -1,9 +1,42 @@
-import { runSolution } from '../utils.ts';
+import {runSolution} from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day9a(data: string[]) {
+  data.pop()
   console.log(data);
-  return 0;
+  const input = data[0]
+
+
+  const disk_layout: number[] = [];
+  let next_id = 0
+  for (let x = 0; x < input.length; x++) {
+    const item_size = +(input[x]);
+    if (x % 2 == 1) {
+      for (let y = 0; y < item_size; y++) {
+        disk_layout.push(NaN);
+      }
+    } else {
+      for (let y = 0; y < item_size; y++) {
+        disk_layout.push(next_id);
+      }
+      next_id++
+    }
+  }
+  console.log(disk_layout);
+  for (let x = 0; x < disk_layout.length; x++) {
+    console.log(x, disk_layout[x]);
+    if (x > disk_layout.length) break;
+    if (isNaN(disk_layout[x])) {
+      disk_layout[x] = disk_layout.pop()
+      while (isNaN(disk_layout[disk_layout.length-1])) disk_layout.pop()
+    }
+  }
+  console.log(disk_layout);
+  let checksum = 0
+  for (let x = 0; x < disk_layout.length; x++) {
+    checksum += x * disk_layout[x];
+  }
+  return checksum;
 }
 
 await runSolution(day9a);
--- day-9/b.ts
+++ day-9/b.ts
@@ -1,9 +1,62 @@
-import { runSolution } from '../utils.ts';
+import {runSolution} from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day9b(data: string[]) {
+  data.pop()
   console.log(data);
-  return 0;
+  const input = data[0]
+
+
+  const disk_layout: {id: number, size: number}[] = [];
+  const files: {id: number, size: number, index: number}[] = [];
+  const spaces: {size: number, index: number}[] = [];
+  let next_id = 0
+  let index = 0;
+  for (let x = 0; x < input.length; x++) {
+    const item_size = +(input[x]);
+    if (x % 2 == 1) {
+      spaces.push({size: item_size, index: index})
+      for (let y = 0; y < item_size; y++) {
+        disk_layout.push({id: NaN, size: item_size});
+      }
+    } else {
+      files.push({id: next_id, size: item_size, index: index})
+      for (let y = 0; y < item_size; y++) {
+        disk_layout.push({id: next_id, size: item_size});
+      }
+      next_id++
+    }
+    index += item_size
+  }
+
+  const start_files = JSON.parse(JSON.stringify(files));
+  console.log(files);
+  console.log(start_files);
+  console.log(spaces);
+  for (let f = start_files.length-1; f >= 0; f--) {
+    console.log("Move file", start_files[f]);
+    for (let space = 0; space < spaces.length; space++) {
+      if ((spaces[space].size >= files[f].size)
+        && spaces[space].index < files[f].index ) {
+        console.log("Can move ", files[f], "to", spaces[space]);
+        files[f].index = spaces[space].index;
+        spaces[space].size -= files[f].size;
+        spaces[space].index += files[f].size;
+        break;
+      }
+    }
+  }
+  console.log(files);
+
+
+  let checksum = 0
+  for (let x = 0; x < files.length; x++) {
+    //console.log(files[x]);
+    for (let y = 0; y < files[x].size; y++) {
+      checksum += (files[x].index + y) * files[x].id;
+    }
+  }
+  return checksum;
 }
 
 await runSolution(day9b);
