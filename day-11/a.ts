820e699c3f92b14bd13f53ce56cfddf4b16d2db9
Anders Arnholm <anders.arnholm@hiq.se>
2024-12-11 12:26:25 +0100

    day 11

--- day-11/a.ts
+++ day-11/a.ts
@@ -2,8 +2,35 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day11a(data: string[]) {
-  console.log(data);
-  return 0;
+  data.pop()
+  //console.log(data);
+  const input = data[0].split(' ')
+  console.log(input);
+
+  function do_step(input: string[]) {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] == '0') {
+        input[i] = '1'
+      } else if (input[i].length % 2 == 0) {
+        const part1 = input[i].substring(0, input[i].length/2);
+        const part2 = (+(input[i].substring(input[i].length/2))).toString();
+        // input.splice(i, 0, second);
+        input[i] = part2
+        input.splice(i, 0, part1)
+        i++
+      } else {
+        const val = +(input[i])
+        input[i] = (2024 * val).toString()
+        // console.log("input[%d] = (2024 * %d)", i, val)
+      }
+    }
+  }
+
+  for (let i = 0; i < 25; i++) {
+    do_step(input);
+    console.log(input);
+  }
+  return input.length;
 }
 
 await runSolution(day11a);
--- day-11/b.ts
+++ day-11/b.ts
@@ -2,8 +2,50 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day11b(data: string[]) {
-  console.log(data);
-  return 0;
+  data.pop()
+  //console.log(data);
+  const input = data[0].split(' ')
+  console.log(input);
+
+  function blink_stone(str: string) {
+    if (str == '0') return ['1']
+    if (str.length % 2 == 0) {
+      const part1 = str.substring(0, str.length/2);
+      const part2 = (+(str.substring(str.length/2))).toString();
+      return [part1, part2]
+    }
+    return [(2024 * (+(str))).toString()]
+  }
+
+  const cache = ({})
+  function step_number(start: string, iterations: number): number {
+    const key = JSON.stringify({start, iterations});
+    if (key in cache) {
+      return cache[key];
+    }
+    if (iterations == 0) {
+      cache[key] = 1;
+      return 1
+    }
+    const blink = blink_stone(start)
+    if (blink.length == 2) {
+      const value = step_number(blink[0], iterations-1) + (step_number(blink[1], iterations-1));
+      cache[key] = value;
+      return value;
+    } else {
+      const value = step_number(blink[0], iterations-1);
+      cache[key] = value;
+      return value
+    }
+  }
+
+  let accumulator = 0
+  for (let i = 0; i < input.length; i++) {
+    const into = step_number(input[i], 75)
+    accumulator += into
+  }
+
+  return accumulator;
 }
 
 await runSolution(day11b);
